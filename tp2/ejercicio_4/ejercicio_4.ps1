225f0af6bbd392533639298b05328cb508ff6c9f
nicofabregues <nicolasfab8@gmail.com>
2020-05-15 10:57:24 -0300

    Se sube Ejercicio 4

---
+++ tp2/ejercicio_4/ejercicio_4.ps1
@@ -0,0 +1,81 @@
+# ejercicio_4.ps1 | Trabajo Práctico 2) Ejercicio 4) | Primera entrega
+# Maximiliano José Bernardo | 41912800
+# Nicolás Agustín Fábregues | 41893896
+# Joela Belén Kachanosqui | 41917556
+# Alejandro Nicolás Tacchella | 41893930
+# Tomás Martín Vera | 41988332
+
+Param(
+    #Validacion de Parametros: Directorio y DirectorioZip tienen que ser enviados obligatoriamente y no ser vacios.
+    #Empresa en cambio puede no estar o ser vacia. Si es vacia se toma como que no estuviese.
+    [Parameter( mandatory = $True)]
+    [ValidateNotNullOrEmpty()]
+    [string] $Directorio,
+
+    [Parameter( mandatory = $True)]
+    [ValidateNotNullOrEmpty()]
+    [string] $DirectorioZip,
+
+    [Parameter( mandatory = $False)]
+    [string] $empresa
+
+)
+
+$existe1 = Test-Path -Path $Directorio -PathType container
+$existe2 = Test-Path -Path $DirectorioZip -PathType container
+
+if( $existe1 -ne $True ){
+    Write-Error "El directorio o directorioZip proporcionado no existe!"
+    exit 1
+}
+if( $existe2 -ne $True ){
+    Write-Error "El directorio o directorioZip proporcionado no existe!"
+    exit 1
+}
+
+if( !$empresa ){
+    $regex_log='[a-zA-Z]+\-[0-9]+\.log$'
+}
+else{
+    $regex_log="$empresa\-[0-9]+\.log$"
+}
+
+$arch_validos = Get-ChildItem $Directorio | Where-Object Name -Match $regex_log | Select-Object -ExpandProperty "Name"
+
+$empresas = @{}
+foreach ($item in $arch_validos) {
+    if ( ! $empresas.Contains( $item.Split('-')[0] )) {
+        $empresas.add( $item.Split('-')[0], "" )
+    } 
+}
+
+# Recorro las empresas
+foreach ($emp in $empresas.keys)
+{
+    #Comprimo los archivos, agrego la opcion -Update para que si llega a existir ya el archivo.zip, no lo borre sino que lo actualize
+    Get-ChildItem $Directorio | Where-Object Name -Match "$emp\-[0-9]+\.log$" | Compress-Archive -Update -DestinationPath "$DirectorioZip/$emp.zip"
+    
+    # Hago un rm de los archivos del directorio de origen solo si fue exitosa la compresión de los archivos
+    if( $? ) {
+        Get-ChildItem $Directorio | Where-Object Name -Match "$emp\-[0-9]+\.log$" | Remove-Item 
+    }
+}
+
+
+<#
+.Synopsis
+    Este script omprime los logs en el directorio que se envía como parámetro, en un directorio especificado.
+.Description
+    El script ejercicio_4.ps1 recibe como parámetros un directorio, en el cual se encuentran archivos de log, 
+    un directorio adonde se comprimiran y, opcionalmente el nombre de la empresa de la cual se buscaran sus logs.
+.Example
+    ./ejercicio_4.ps1 -Directorio logs -DirectorioZip compri
+.Example
+    ./ejercicio_4.ps1 -Directorio logs -DirectorioZip compri -empresa movistar
+.Notes
+    Parámetros:
+    -Directorio: Directorio en el que se encuentran los archivos de log. Puede ser una ruta relativa o absoluta.
+    -DirectorioZip: Directorio en el que se generarán los archivos comprimidos de los clientes.
+    -Empresa: Nombre de la empresa a procesar. (opcional)
+    Los nuevos archivos se agregan al ZIP existente para no perder los ya procesados.
+#>
