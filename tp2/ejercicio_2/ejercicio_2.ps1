aabfef40f7559df8b3f5359ef131fa07f86d05b5
nicofabregues <nicolasfab8@gmail.com>
2020-05-18 15:19:49 -0300

    Se agrega help, actualiza el out y se prueba el Format-List [incompleto]

--- tp2/ejercicio_2/ejercicio_2.ps1
+++ tp2/ejercicio_2/ejercicio_2.ps1
@@ -35,6 +35,7 @@ $cantLlamadasUsuario = @{}
 $duracionLlamada = @{}
 $cantLlamadasSemana = 0
 $duracionLlamadasSemana = 0
+
 $file_data | ForEach-Object {
     if ( $vecLlamada.Contains($_.Usuario) ){
         $diaInicial = $vecLlamada[$_.Usuario].Split(" ")[0].Split("-")[2]
@@ -69,29 +70,33 @@ $file_data | ForEach-Object {
 }
 
  #Punto 1
- Write-Output "PUNTO 1:"
- Write-Output "Promedio de tiempo de las llamadas realizadas por día" 
- foreach ($dia in $totalTiempo.keys){
-   Write-Output "Promedio de tiempo del dia $dia son: $($totalTiempo[$dia]/$cantLlamadas[$dia])"
+ Write-Output "`nPUNTO 1:"
+ Write-Output "`Promedio de tiempo de las llamadas realizadas por día`n" 
+ foreach ($dia in $totalTiempo.keys.GetEnumerator() | Sort-Object ){
+     $prom = $totalTiempo[$dia]/$cantLlamadas[$dia]
+     $prom = '{0:hh\:mm\:ss}' -f $prom
+    "`t$($dia): `t$($prom)"| Format-List  
+    #Write-Output "`tPromedio de tiempo del dia $dia son: $($totalTiempo[$dia]/$cantLlamadas[$dia])"
  }
 
  #Punto 2
- Write-Output "PUNTO 2:"
- Write-Output "Promedio de tiempo y cantidad por usuario por día" 
- foreach ($usuarioDia in $tiempoUsuarioDia.keys){
+ Write-Output "`nPUNTO 2:"
+ Write-Output "Promedio de tiempo y cantidad por usuario por día`n" 
+ foreach ($usuarioDia in $tiempoUsuarioDia.keys.GetEnumerator() | Sort-Object){
     $usuarioDia_list = $usuarioDia.Split("-")
-    Write-Output "La cantidad de llamadas del dia $($usuarioDia_list[1]) del usuario $($usuarioDia_list[0]) son: $($cantUsuarioDia[$usuarioDia])"
-    Write-Output "El promedio de tiempo es $($tiempoUsuarioDia[$usuarioDia]/$cantUsuarioDia[$usuarioDia])"
+    "`tUsuario: $($usuarioDia_list[0]) Dia: $($usuarioDia_list[1]) Llamadas: $($cantUsuarioDia[$usuarioDia]) Promedio de Tiempo: $($tiempoUsuarioDia[$usuarioDia]/$cantUsuarioDia[$usuarioDia])" | Format-List -GroupBy $usuarioDia_list[0]
+  # Write-Output "`tLa cantidad de llamadas del dia $($usuarioDia_list[1]) del usuario $($usuarioDia_list[0]) son: $($cantUsuarioDia[$usuarioDia])"
+   # Write-Output "`tY su promedio de tiempo es $($tiempoUsuarioDia[$usuarioDia]/$cantUsuarioDia[$usuarioDia])`n"
 }
 
 #Punto 3
 $flag = 0
-Write-Output "PUNTO 3:"
-Write-Output "Los tres usuarios con más llamadas son:"
+Write-Output "`nPUNTO 3:"
+Write-Output "Los tres usuarios con más llamadas son:`n"
 foreach ($item in $cantLlamadasUsuario.GetEnumerator() | Sort-Object -Property value -Descending){
     if ( $flag -lt 3 ){
         $flag++
-        Write-Output "$($flag)-El usuario $($item.name) con $($item.value)"
+        Write-Output "`t$($flag)-El usuario $($item.name) con $($item.value)"
     }
     else{
         break
@@ -99,8 +104,8 @@ foreach ($item in $cantLlamadasUsuario.GetEnumerator() | Sort-Object -Property v
 }
 
 #Punto 4 - Parte 1
-Write-Output "PUNTO 4:"
-Write-Output "Cantidad de llamadas por debajo de la media diaria:"
+Write-Output "`nPUNTO 4:"
+Write-Output "Cantidad de llamadas por debajo de la media diaria:`n"
 $callsBelowMedia = @{}
 foreach ($dia in $totalTiempo.keys){
     $promedioDia = $totalTiempo[$dia]/$cantLlamadas[$dia]
@@ -112,7 +117,7 @@ foreach ($dia in $totalTiempo.keys){
             }
         }
     }
-    Write-Output "Para el dia $dia fueron $($callsBelowMedia[$dia]) llamadas por debajo del promedio del día."
+    Write-Output "`tPara el dia $dia fueron $($callsBelowMedia[$dia]) llamadas por debajo del promedio del día."
 }
 
 #Punto 4 - Parte 2
@@ -125,4 +130,24 @@ foreach ($key in $duracionLlamada.keys){
     }
 }
 $user = $callsBelowMediaUser.GetEnumerator() | Sort-Object -Property value -Descending -Top 1
-Write-Output "El usuario con más llamadas con duración por debajo de la media semanal es: $($user.name) con $($user.value)"
+Write-Output "`nEl usuario con más llamadas con duración por debajo de la media semanal es: $($user.name) con $($user.value)`n`n"
+
+<#
+.Synopsis
+    Este script lee el log proporcionado a traves de un path enviado y realiza distintas operaciones.
+.Description
+    El script ejercicio_2.ps1 recibe como parametro un el path de un log.
+    Se pide obtienen y muestran por pantalla los siguientes datos uno debajo del otro:
+        ▪ Promedio de tiempo de las llamadas realizadas por día.
+        ▪ Promedio de tiempo y cantidad por usuario por día.
+        ▪ Los 3 usuarios con más llamadas en la semana.
+        ▪ Cuántas llamadas no superan la media de tiempo por día y el usuario que tiene más llamadas por debajo de la media en la semana.
+    Parámetros:
+        • -Path: Directorio en el que se encuentran los archivos de log. Puede ser una ruta relativa o absoluta.
+    Formato del registro de llamada (log):
+        • Fecha y Hora: Formato YYYY-MM-DD HH:mm:ss
+        • Usuario que realiza la llamada.
+        • Separador: “ _ “ (espacio, guión bajo, espacio)
+.Example
+    ./ejercicio_2.ps1 -Path logs/telefonica-1.log
+#>
