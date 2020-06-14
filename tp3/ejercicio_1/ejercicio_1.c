984a9aaa178b124f8678d40e0edd1d99d989300d
nicofabregues <nicolasfab8@gmail.com>
2020-06-14 14:39:37 -0300

    Se agrega Makefile - Se agrega Help - Se valida que se ingrese un entero

--- tp3/ejercicio_1/ejercicio_1.c
+++ tp3/ejercicio_1/ejercicio_1.c
@@ -5,6 +5,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+# ejercicio_1.c | Trabajo Práctico 3) Ejercicio 1) | Primera Entrega
+# Maximiliano José Bernardo | 41912800
+# Nicolás Agustín Fábregues | 41893896
+# Joela Belén Kachanosqui | 41917556
+# Alejandro Nicolás Tacchella | 41893930
+# Tomás Martín Vera | 41988332
+*/
+
 void mostrar_procesos(pid_t vec[], int nodos[],int tam){
     for(int i = tam - 1; i >= 0; i--){
         if(vec[i] != 0){
@@ -31,19 +40,49 @@ void init_vector(pid_t vec[],int tam){
     }
 }
 
+void help(){
+    printf("\n######    HELP Ejercicio_1    ######\n\n");
+    printf("Este programa dado un parámetro N de entrada \n");
+    printf("recrea el siguiente árbol de procesos.\n\n");
+    printf("            1               \n");
+    printf("        /       4   -   7   \n");
+    printf("    /       /               \n");
+    printf(" 0  -   2   -   5           \n");
+    printf("    \\                      \n");
+    printf("        3   -   6           \n\n");
+    printf("Cada proceso imprimira su pid y el de toda su ascendencia.\n");
+    printf("Y ademas creara N jerarquías de procesos hijos adicionales.\n");
+    printf("\n");
+    printf("Para ejecutar el programa: \"./Ejercicio_1 N\",\n");
+    printf("donde N debe ser un entero mayor o igual a cero.\n\n");
+
+    
+}
+
 int main(int argc, char* argv[]){
 
     int l;
+    char info[]="Para más información ejecute Ejercicio_1 -h o Ejercicio_1 --help.\n";
+
+    if( !strcmp("-h", argv[1]) || !strcmp("-?", argv[1]) || !strcmp("--help", argv[1]) ){
+        help();
+        return EXIT_SUCCESS;
+    }
 
     if(argc != 2){
-        perror("El programa debe recibir un único parámetro!");
-        exit(1);
+        printf("\n¡ERROR!: El programa debe recibir un único parámetro\n");
+        printf ("%s\n", info);
+        return EXIT_FAILURE;
     }
-    
+
     l = atoi(argv[1]);
-    if(l < 0){
-        perror("La cantidad de procesos debe ser igual o mayor a cero!");
-        exit(1);
+    if( strcmp("0", argv[1]) ){
+
+        if(l <= 0){
+            printf ("\n¡ERROR!: Ingrese un entero mayor o igual a cero\n");
+            printf ("%s\n", info);
+            return EXIT_FAILURE;
+        }
     }
 
     const int tam = 4+l;
