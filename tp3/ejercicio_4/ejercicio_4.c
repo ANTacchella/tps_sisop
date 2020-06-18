1cec315921c02e914e1bff5b240371e1ac2ad750
ANTacchella <alejandronicolastacchella@gmail.com>
2020-06-17 21:34:09 -0300

    [UPD]Avances en comunicación de Control y Registro

--- tp3/ejercicio_4/ejercicio_4.c
+++ tp3/ejercicio_4/ejercicio_4.c
@@ -5,41 +5,83 @@
 #include <sys/types.h> 
 #include <unistd.h>
 #include <stdlib.h> 
-#include <signal.h> 
+#include <signal.h>
+#include <wait.h>
 
 /*
-# ejercicio_4.c | Trabajo Práctico 3) Ejercicio 4) | Primera Entrega
-# Maximiliano José Bernardo | 41912800
-# Nicolás Agustín Fábregues | 41893896
-# Joela Belén Kachanosqui | 41917556
-# Alejandro Nicolás Tacchella | 41893930
-# Tomás Martín Vera | 41988332
+ejercicio_4.c | Trabajo Práctico 3) Ejercicio 4) | Primera Entrega
+Maximiliano José Bernardo | 41912800
+Nicolás Agustín Fábregues | 41893896
+Joela Belén Kachanosqui | 41917556
+Alejandro Nicolás Tacchella | 41893930
+Tomás Martín Vera | 41988332
 */
 
-pid_t pid_control = -1 ; //Global
-pid_t pid_registro = -1 ; //Global
+typedef struct ps_struct
+{
+    int pid;
+    float cpu;
+    float mem;
+    char comm[20];
+}ps_struct;
 
-void signalHandler( int sig ){
+pid_t pid_control = -1; //Global
+pid_t pid_registro = -1; //Global
+int control_status;
+int reg_status;
+FILE *cmd;
+char *control_fifo = "./control_fifo";
+
+void signalHandler(int sig){
     //Aca deberiamos matar a los hijos.
-
-    return EXIT_SUCCESS;
+    kill(pid_control,SIGKILL);
+    kill(pid_registro,SIGKILL);
+    exit(EXIT_SUCCESS);
 }
 
+void char_to_ps_struct(const char *ps_line, ps_struct *ps_info){
+    int res;
+    res = sscanf(ps_line, "%d %f %f %[^;]", &ps_info->pid, &ps_info->cpu, &ps_info->mem, ps_info->comm);
+    if(res == EOF){
+        printf("¡ERROR! Sscanf failure: No se pudo leer el string del proceso\n");
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main() 
 { 
     //signal handler SIGUSR1
-    signal( SIGUSR1, &signalHandler );
-
+    signal(SIGUSR1, &signalHandler);
+    mkfifo(control_fifo, 0666);
     //crear hijos y dejarlos en segundo plano
-    
     pid_control = fork(); //Creo a Control
     if (pid_control == 0)
-    {
+    {   
+        ps_struct ps_info;
+        char ps[41];
+        char ps_line[40];
+        int pid;
+        float cpu;
+        float mem;
+        char comm[20];
+        int file_desc;
         //Codigo Hijo Control
-        while( 1 ){
-
-
+        while(1){
+            file_desc = open(control_fifo, O_WRONLY);
+            sprintf(ps, "ps --no-headers -Ao pid,%%cpu,%%mem,comm");
+            cmd = popen(ps, "r");
+            if (cmd == NULL) {
+                printf("¡ERROR! Popen failure: No se pudo leer la salida de ps\n");
+                exit(EXIT_FAILURE);
+            }
+            while (fgets(ps_line, sizeof(ps_line), cmd) != NULL) {
+                //printf("%s\n", ps_line);
+                char_to_ps_struct(ps_line, &ps_info);
+                //printf("%d %f %f %s\n", (&ps_info)->pid, (&ps_info)->cpu, (&ps_info)->mem, (&ps_info)->comm);
+                write(file_desc, ps_line, sizeof(ps_line));
+            }
+            pclose(cmd);
+            close(file_desc);
             sleep(1);
         }
     }
@@ -47,73 +89,22 @@ int main()
         pid_registro = fork(); //Creo a Registro
         if (pid_registro == 0)
         {
+            char ps_line[40];
+            int file_desc;
             //Codigo Hijo Registro
-            while( 1 ){
-
-                
-            sleep(1);
+            while(1){
+                file_desc = open(control_fifo, O_RDONLY);
+                while(read(file_desc, ps_line, sizeof(ps_line))){
+                    printf("Registro: %s\n", ps_line);
+                }
+                close(file_desc);
+                sleep(1);
+            }
         }
-        }
-    }
+    }   
     
-    
-    while( 1 ){
-
-
-        sleep(1);
-    }
-    
-    
+    waitpid(pid_control, &control_status, 0);
+    waitpid(pid_registro, &reg_status, 0);
     //podriamos crear FIFO aca tambien
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    int fd; 
-  
-    // FIFO file path 
-    char * myfifo = "/tmp/myfifo"; 
-  
-    // Creating the named file(FIFO) 
-    // mkfifo(<pathname>, <permission>) 
-    mkfifo(myfifo, 0666); 
-  
-    char arr1[80], arr2[80]; 
-    while (1) 
-    { 
-        // Open FIFO for write only 
-        fd = open(myfifo, O_WRONLY); 
-  
-        // Take an input arr2ing from user. 
-        // 80 is maximum length 
-        fgets(arr2, 80, stdin); 
-  
-        // Write the input arr2ing on FIFO 
-        // and close it 
-        write(fd, arr2, strlen(arr2)+1); 
-        close(fd); 
-  
-        // Open FIFO for Read only 
-        fd = open(myfifo, O_RDONLY); 
-  
-        // Read from FIFO 
-        read(fd, arr1, sizeof(arr1)); 
-  
-        // Print the read message 
-        printf("User2: %s\n", arr1); 
-        close(fd); 
-    }
-
-
-
-
-
-
     return 0; 
-} 
+}
